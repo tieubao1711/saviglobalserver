62f2cebd0adae03ee8d36ee4b0c3a4c86c2e1632
tieubao1711 <quangtran1711dn@gmail.com>
2024-12-02 18:24:24 +0700

    cors2

--- src/index.ts
+++ src/index.ts
@@ -18,37 +18,21 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 1711;
 
-// Cấu hình CORS
-const corsOptions = {
-  origin: [
-    'https://saviglobal.xyz', // Cho phép từ production domain
-    'http://localhost:3000',  // Cho phép từ local dev domain
-  ],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Các phương thức mà bạn cho phép
-  allowedHeaders: [
-    'Content-Type', 
-    'Authorization', 
-    'Access-Control-Allow-Headers', 
-    'Access-Control-Allow-Methods',
-  ], // Các header cần thiết
-  credentials: true, // Cho phép gửi cookie từ client
-  preflightContinue: false,
-  optionsSuccessStatus: 204, // Đặt status code cho response preflight (OPTIONS)
-};
-
-app.use(cors(corsOptions)); // Cấu hình CORS middleware với options đã cập nhật
-
-// Session middleware
 app.use(session({
   secret: process.env.CAPTCHA_SECRET || 'captcha_secret',
   resave: false,
   saveUninitialized: true,
   cookie: {
-    secure: false, // Đặt true nếu sử dụng HTTPS
+    secure: false, // Đặt `true` nếu sử dụng HTTPS
     httpOnly: false, // Cho phép truy cập từ phía client
     sameSite: 'lax' // Hoặc 'none' nếu sử dụng HTTPS
   }
 }));
+
+// cors?
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // MongoDB Connection
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/saviglobal';
@@ -60,11 +44,13 @@ mongoose
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with MongoDB!');
+    res.send('Hello, TypeScript with MongoDB!');
 });
 
 app.use('/api/admin', adminRoutes);
+
 app.use('/api', testRoutes);
+
 app.use('/api', cartRoutes);
 app.use('/api', captchaRoutes);
 app.use('/api/auth', authRoutes);
